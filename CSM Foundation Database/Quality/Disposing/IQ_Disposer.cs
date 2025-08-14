3a3cd89aa5ec0610eb68cf763e8d92031a344bd1
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-08-13 19:55:55 -0700

    initialized package

---
+++ CSM Foundation Database/Quality/Disposing/IQ_Disposer.cs
@@ -0,0 +1,11 @@
+﻿namespace CSM_Foundation_Database.Quality.Disposing;
+
+/// <summary>
+///     [Interface] for [Quality] purposes [Disposer] implementations.
+/// </summary>
+/// <remarks>
+///     This Disposer only must be used on [Quality]/[Testing] strictly purposes.
+/// </remarks>
+public interface IQ_Disposer
+    : IDisposer {
+}
